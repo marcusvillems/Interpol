a81d5b4cdffd4d17954769354c57f414a8fccc30
Marcus <marcus.villems@gmail.com>
2023-05-05 18:41:49 +0300

    Added files to new repository

---
+++ HTML/store.js
@@ -0,0 +1,71 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
+
+function ready () {
+    var removeCartItemButton = document.getElementsByClassName("btn-danger")
+console.log(removeCartItemButton)
+for (var i = 0; i < removeCartItemButton.length; i++) {
+    var button = removeCartItemButton[i]
+    button.addEventListener("click", removeCartItem)
+    }
+}
+
+var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+}
+
+var addToCartButtons = document.getElementsByClassName('shop-item-button')
+for (var i = 0; i < quantityInputs.length; i++) {
+    var button = addToCartButtons[i]
+    button.addEventListener('click', addToCartClicked)
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+}
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    cartItems.append('cartRow')
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click')  
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i]
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+    var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+    var price = parseFloat(priceElement.innerText.replace('$', ''))
+    var quantity = quantityElement.value
+    total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
